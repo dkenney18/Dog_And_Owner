0ee4d6bf7ed827c1fb2031c1aa632f91973269db
Dev K <bond98041@gmail.com>
2021-06-02 19:41:01 +0000

    Added sample test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Math', function() {
+  describe('#Add 2 + 2', function() {
+    it('should return 4 when 2 + 2 are added together', function() {
+      assert.equal(2 + 2, 4);
+    });
+  });
+});
