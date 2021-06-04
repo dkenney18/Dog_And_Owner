f914e740016708e940c597fe02fa514ee021534d
Devin Kenney <bond98041@gmail.com>
2021-06-04 15:04:09 -0400

    added tests for Owner

--- test/test.js
+++ test/test.js
@@ -1,115 +1,167 @@
-const { Breed } = require("../classes/base/Breed.js")
-const { Animal } = require("../classes/Animal.js")
-const { Owner } = require("../classes/Owner.js")
-const { Food } = require("../classes/base/Food")
+const {
+  Breed
+} = require("../classes/base/Breed.js")
+const {
+  Animal
+} = require("../classes/Animal.js")
+const {
+  Owner
+} = require("../classes/Owner.js")
+const {
+  Food
+} = require("../classes/base/Food")
 
-class Dog extends Animal { }
-class Cat extends Animal{ }
+class Dog extends Animal {}
 
 var assert = require('assert');
-var should = require('chai').should();
+var should = require('chai').should()
+
 
 //setup
 var pug = new Breed("Pug", 0)
 var dog = new Dog("Sam", pug)
+var devin = new Owner("Devin")
 
-describe('Breed', function() {
-  describe('#Name', function() {
-    it('should return Name passed', function() {
+describe('Breed', function () {
+  describe('#Name', function () {
+    it('should return Name passed', function () {
       assert.strictEqual(pug.name, "Pug")
     });
   });
 
-  describe('#Name', function() {
-    it('should return Name as string', function() {
+  describe('#Name', function () {
+    it('should return Name as string', function () {
       var pug = new Breed("Pug", 0)
       assert.strictEqual(typeof pug.name, typeof "Pug")
     });
   });
 
-  describe('#HP', function() {
-    it('should return HP passed', function() {
+  describe('#HP', function () {
+    it('should return HP passed', function () {
       var pug = new Breed("Pug", 0)
       assert.strictEqual(pug.hp, 0)
     });
   });
 
-  describe('#HP', function() {
-    it('should return HP as int', function() {
+  describe('#HP', function () {
+    it('should return HP as int', function () {
       var pug = new Breed("Pug", 0)
       assert.strictEqual(typeof pug.hp, typeof 0)
     });
   });
 });
 
-describe('Animal', function() {
-  describe('#Name', function() {
-    it('should return Name passed', function() {
+describe('Animal', function () {
+  describe('#Name', function () {
+    it('should return Name passed', function () {
       assert.strictEqual(dog.name, "Sam")
     })
   })
 
-  describe('#Name', function() {
-    it('should return Name as String', function() {
+  describe('#Name', function () {
+    it('should return Name as String', function () {
       assert.strictEqual(typeof dog.name, typeof "Sam")
     })
   })
 
-  describe('#Breed', function() {
-    it('should return Name passed', function() {
+  describe('#Breed', function () {
+    it('should return Name passed', function () {
       assert.strictEqual(pug.name, "Pug")
     });
   });
 
-  describe('#Breed', function() {
-    it('should return Name as string', function() {
+  describe('#Breed', function () {
+    it('should return Name as string', function () {
       assert.strictEqual(typeof pug.name, typeof "Pug")
     });
   });
 
-  describe('#Breed', function() {
-    it('should return HP passed', function() {
+  describe('#Breed', function () {
+    it('should return HP passed', function () {
       assert.strictEqual(pug.hp, 0)
     });
   });
 
-  describe('#Breed', function() {
-    it('should return HP as int', function() {
+  describe('#Breed', function () {
+    it('should return HP as int', function () {
       assert.strictEqual(typeof pug.hp, typeof 0)
     });
   });
 
-  describe('#Owner', function() {
-    it('should return null when not assigned an owner', function() {
+  describe('#Owner', function () {
+    it('should return null when not assigned an owner', function () {
       assert.strictEqual(typeof dog.owner, typeof null)
     });
   });
 
-  describe('#Owner', function() {
-    it('can add owner', function() {
-      var dog1 = new Dog("lin", new Breed("Pug", 0))
-      var devin = new Owner("Devin")
-      dog1.setOwner(devin)
-      assert.strictEqual(dog1.isOwned, true)
-      assert.strictEqual(dog1.owner.name, "Devin")
+  describe('#Owner', function () {
+    it('can add owner', function () {
+      dog.setOwner(devin)
+      assert.strictEqual(dog.isOwned, true)
+      assert.strictEqual(dog.owner.name, "Devin")
     });
   });
 
-  describe('#Owner', function() {
-    it('isOwned is set when new owner is added', function() {
-      var dog1 = new Dog("lin", new Breed("Pug", 0))
-      var devin = new Owner("Devin")
-      dog1.setOwner(devin)
-      assert.strictEqual(dog1.isOwned, true)
+  describe('#Owner', function () {
+    it('isOwned is set when new owner is added', function () {
+      dog.setOwner(devin)
+      assert.strictEqual(dog.isOwned, true)
     });
   });
 
-  describe('#Owner', function() {
-    it('owner is not null', function() {
-      var dog1 = new Dog("lin", new Breed("Pug", 0))
-      var devin = new Owner("Devin")
-      dog1.setOwner(devin)
-      should.exist(dog1.owner)
+  describe('#Owner', function () {
+    it('owner is exsists when added', function () {
+      dog.setOwner(devin)
+      should.exist(dog.owner)
+    });
+  });
+
+  describe('#Owner', function () {
+    it('owner name matches name passed', function () {
+      dog.setOwner(devin)
+      assert.strictEqual(dog.owner.name, "Devin")
     });
   });
 })
+
+describe('Owner', function () {
+  describe('#Name', function () {
+    it('should return Name passed', function () {
+      assert.strictEqual(devin.name, "Devin")
+    });
+  });
+
+  describe('#Name', function () {
+    it('should return Name as string', function () {
+      assert.strictEqual(typeof devin.name, typeof "Devin")
+    });
+  });
+
+  describe('#Array', function () {
+    it('should return empty array', function () {
+      assert.strictEqual(devin.animals.length, 0)
+    });
+  });
+
+  describe('#Array', function () {
+    it('should return length 1 when animal added', function () {
+      var claire = new Owner("Claire")
+      var poodle = new Breed("Poodle")
+      var dog1 = new Dog("lam", poodle)
+      claire.addAnimal(dog1)
+      assert.strictEqual(claire.animals.length, 1)
+    });
+  });
+
+  describe('#ID', function () {
+    it('should return ID as string', function () {
+      assert.strictEqual(typeof devin.id, typeof "")
+    });
+  });
+
+  describe('#ID', function () {
+    it('ID length should be 50', function () {
+      assert.strictEqual(devin.id.length, 50)
+    });
+  });
+});
